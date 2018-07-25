14362115ec6cd7ca5cde1a92c2e48b375dd7a6df
gonzalo <gonzalo@gonzalo>
2018-07-25 23:41:31 +0000

    initial sadness

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sadness
+
+A simple .php file to print random phrases from a sadness person.
+Maybe in a future, can print phrases from a db like sqlite3, for now, I am to sad for that.
+
+# What you need?
+
+A web server, php and sadness.
---
+++ sadness.php
@@ -0,0 +1,13 @@
+<?php
+
+$sadness = array(
+	"A teardrop on earth summons the King of heaven.",
+	"Nothing is more sad than the death of an illusion.",
+	"Tears come from the heart and not from the brain.",
+	"Tears are the summer showers to the soul.",
+	"Sadness is also a kind of defence."
+	);
+
+echo $sadness[array_rand($sadness)];
+
+?>
